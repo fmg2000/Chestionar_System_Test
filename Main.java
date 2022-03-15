08dbaa4900c500a5e204b345987a66b22c29d5b9
fmg2000 <62462553+fmg2000@users.noreply.github.com>
2022-03-15 19:16:34 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import java.io.*;
+import java.lang.reflect.Array;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+
+       Quiz quiz = new Quiz();
+
+
+    }
+
+}
